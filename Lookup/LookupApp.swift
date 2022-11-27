fadae2d2a31d06dc5b9350b7ae0aa3bb9528e31d
Adrian Bolinger <adrian.bolinger@me.com>
2022-11-27 14:16:27 -0500

    Initial Commit

---
+++ Lookup/LookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookupApp.swift
+//  Lookup
+//
+//  Created by Adrian Bolinger on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
